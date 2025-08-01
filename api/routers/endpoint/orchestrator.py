9a237e5b075bb9a5dc77b5fdc1c1812c6bead1d1
Abhinav KM <abhinav.km@thoughtminds.io>
2025-08-01 13:31:35 +0530

    feat: Use BaseModel

--- api/routers/endpoint/orchestrator.py
+++ api/routers/endpoint/orchestrator.py
@@ -8,12 +8,10 @@ router = APIRouter()
 SessionDep = Annotated[Session, Depends(db.get_session)]
 
 
-@router.post("/identify_intent/")#, response_model=schema.OrchestrationResponse)
-def identify_intent(session: SessionDep, data: schema.OrchestrationQuery) :#-> schema.OrchestrationResponse:
+@router.post("/identify_intent/", response_model=schema.OrchestrationResponse)
+def identify_intent(session: SessionDep, data: schema.OrchestrationQuery) -> schema.OrchestrationResponse:
     query = data.query
     response = agent.orchestrator_graph.invoke({"query": query})
     category = response["category"]
-    print(f"Category: {category}")
-    # response = schema.OrchestrationResponse(category=intent)
-    # return response
-    return category
+    response = schema.OrchestrationResponse(category=category)
+    return response

d18e184b35882789e0aa328c77b40ad92548e68e
Abhinav KM <abhinav.km@thoughtminds.io>
2025-08-01 13:28:31 +0530

    feat: Import model

--- api/schema/__init__.py
+++ api/schema/__init__.py
@@ -3,4 +3,5 @@ from .api import *
 from .log import AuditLog, RequestData
 from .summarization import SummaryRequest, SummaryResponse
 from .task_execution import TaskRequest, TaskResponse
+from .orchestrator import OrchestrationQuery, OrchestrationResponse
 from .metadata import DashboardStats

6fd24cd31ce2b60d1c8488a6661de5fa308045be
Abhinav KM <abhinav.km@thoughtminds.io>
2025-08-06 09:23:15 +0530

    dev: Mock summarization agent usage

--- api/routers/endpoint/summarization.py
+++ api/routers/endpoint/summarization.py
@@ -7,15 +7,30 @@ from sqlmodel import Session
 router = APIRouter()
 SessionDep = Annotated[Session, Depends(db.get_session)]
 
+# Support single, multiple API   call operation (flag?)
 
-@router.post("/get_summary/")#, response_model=schema.SummaryResponse)
-def get_summary(session: SessionDep, data: schema.SummaryRequest):# -> schema.SummaryResponse:
+@router.post("/get_summary/", response_model=schema.SummaryResponse)
+def get_summary(session: SessionDep, data: schema.SummaryRequest) -> schema.SummaryResponse:
     query = data.query
-    # # get summary (make api call, db fetch and summarize with LLM)
-    summary = agent.summarization_graph.invoke({"messages": [{"role": "user", "content": query}]})
-    print(f"SUMMARY: {summary}")
-    # apply content moderation on summary
-    # response = schema.SummaryResponse(summary=summary, content_moderated=False)
-    # # Support single, multiple API call operation (flag?)
-    # return response
-    return "OK"
+    # get summary (make api call, db fetch and summarize with LLM)
+
+    ####
+    initial_state = {
+        "messages": [],
+        "query": query,
+        "table_names": "",
+        "schema": "",
+        "checked_query": "",
+        "query_result": "",
+        "summary": ""
+    }
+    
+    result = agent.summarization_graph.invoke(initial_state)
+    
+    if "summary" in result:
+        content = result["summary"]
+    else:
+        content = ""
+  
+    response = schema.SummaryResponse(summary=content, content_moderated=False)
+    return response

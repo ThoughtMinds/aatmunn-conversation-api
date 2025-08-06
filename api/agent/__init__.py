d4bae44e76bc30b2ee72aa7b5386a3e7c3048e3f
Abhinav KM <abhinav.km@thoughtminds.io>
2025-08-06 14:39:50 +0530

    feat: Add minimal summarization agent

--- api/agent/__init__.py
+++ api/agent/__init__.py
@@ -1,3 +1,5 @@
 from .navigation_graph import navigation_graph
 from .orchestrator_graph import orchestrator_graph
-from .summarization_agent import summarization_graph
+from .summarization_agent import get_summarized_response
+# from ._manual_summarization_graph import summarization_graph
+# from .summarization_agent import summarization_graph

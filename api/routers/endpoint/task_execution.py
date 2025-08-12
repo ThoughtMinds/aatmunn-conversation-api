bebd1747d5a7fb45f3446f66676725e62f56e9ab
Abhinav KM <abhinav.km@thoughtminds.io>
2025-08-12 13:50:40 +0530

    feat: Update task execution to use tool calling LLM + Prompt

--- api/routers/endpoint/task_execution.py
+++ api/routers/endpoint/task_execution.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends
 from api import db, schema, agent
 from typing import Annotated
 from sqlmodel import Session
-from langchain_core.messages import HumanMessage
 from api.core.logging_config import logger
 
 
@@ -11,11 +10,14 @@ SessionDep = Annotated[Session, Depends(db.get_session)]
 
 # Support single, multiple API call operation (flag?)
 
+
 @router.post("/execute_task/", response_model=schema.TaskResponse)
-def execute_task(session: SessionDep, data: schema.TaskRequest) -> schema.TaskResponse:
+def execute_task(
+    session: SessionDep, data: schema.TaskRequest
+) -> schema.TaskResponse:
     query = data.query
     logger.info(f"Task Execution Query: {query}")
-    
-    content = agent.execute_task(messages=[HumanMessage(content=query)])
+
+    content = agent.execute_task(query)
     response = schema.TaskResponse(response=content, status=True)
     return response

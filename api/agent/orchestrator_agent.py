1cf8b47f6b098c41ddf9667ad4a9cb575a15a1bb
Abhinav KM <abhinav.km@thoughtminds.io>
2025-08-21 10:19:23 +0530

    feat: Add renamed agents

---
+++ api/agent/orchestrator_agent.py
@@ -0,0 +1,68 @@
+from typing_extensions import TypedDict
+from langgraph.graph import START, StateGraph
+from api import llm, schema
+from typing import Optional
+
+
+class State(TypedDict):
+    """
+    Represents the state of the graph.
+
+    Attributes:
+        query (str): The question being asked.
+        category (str): The identified category of the query.
+    """
+
+    query: str
+    category: str
+
+
+def identify_intent(state: State) -> State:
+    """
+    Identifies the intent of the query using the LLM.
+
+    Args:
+        state (State): The current state containing the query.
+
+    Returns:
+        State: Updated state with the identified category.
+    """
+    try:
+        chat_model = llm.get_ollama_chat_fallback_model()
+        orchestrator_chain = llm.create_chain_for_task(
+            task="orchestration", llm=chat_model
+        )
+        response = orchestrator_chain.invoke({"query": state["query"]})
+        category = response.content.lower()
+        return {"category": category}
+    except Exception as e:
+        print(f"Failed to get Orchestration due to: {e}")
+        return {"category": "error"}
+
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("identify_intent", identify_intent)
+
+graph_builder.add_edge(START, "identify_intent")
+
+orchestrator_graph = graph_builder.compile()
+
+
+def get_orchestrator_response(
+    query: str, chained: bool = False
+) -> Optional[schema.OrchestrationResponse]:
+    """
+    Generates a Orchestrator response for a given query using a LangGraph workflow.
+
+    Args:
+        query (str): The user's query for navigation.
+        chained (bool): This parameter is ignored, but included for compatibility.
+
+    Returns:
+        schema.OrchestrationResponse: The navigation response, or None if no response is generated.
+    """
+    result = orchestrator_graph.invoke({"query": query})
+    category = result.get("category")
+    response = schema.OrchestrationResponse(category=category)
+    return response

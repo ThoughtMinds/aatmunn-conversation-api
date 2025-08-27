d40bc6b97b2dd9a3a57cd420bdd6d8740935ee69
abhinavkm0 <abhinav.km@thoughtminds.io>
2025-08-27 17:33:17 +0530

    feat: Remove legacy data

--- api/db/__init__.py
+++ api/db/__init__.py
@@ -1,5 +1,3 @@
 from .navigation import *
-from .summarization import *
-from .base import create_db_and_tables, get_session, engine, sqlite_db, populate_summarization_tables
-from .summarization_utils import populate_db_from_json
+from .base import create_db_and_tables, get_session, engine, sqlite_db
 from .navigation_utils import *
